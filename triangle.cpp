d0f3d300596b7b86cab67e79386f04a32dd1758c
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-08-11 23:07:44 +0600

    Update triangle.cpp

--- triangle.cpp
+++ triangle.cpp
@@ -3,16 +3,16 @@
 Triangle::Triangle(double aa, double bb, double cc, double AA, double BB, double CC)
     : a(aa), b(bb), c(cc), A(AA), B(BB), C(CC)
 {
-    name = "Треугольник";
+    name = "РўСЂРµСѓРіРѕР»СЊРЅРёРє";
     sides_count = 3;
     if (A + B + C != 180.0) {
-        throw FigureException("Сумма углов не равна 180");
+        throw FigureException("РЎСѓРјРјР° СѓРіР»РѕРІ РЅРµ СЂР°РІРЅР° 180");
     }
 }
 
 void Triangle::print_info()
 {
     Figure::print_info();
-    cout << "Стороны: a = " << a << ", b = " << b << ", c = " << c
-         << " Углы: A = " << A << ", B = " << B << ", C = " << C << ".\n" << endl;
+    cout << "РЎС‚РѕСЂРѕРЅС‹: a = " << a << ", b = " << b << ", c = " << c
+         << " РЈРіР»С‹: A = " << A << ", B = " << B << ", C = " << C << ".\n" << endl;
 }
