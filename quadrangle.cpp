8ecb2a2dc4142da8faca6344350a433842301b0d
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-08-11 23:06:21 +0600

    Update quadrangle.cpp

--- quadrangle.cpp
+++ quadrangle.cpp
@@ -4,17 +4,19 @@ Quadrangle::Quadrangle(double aa, double bb, double cc, double dd,
                        double AA, double BB, double CC, double DD)
     : a(aa), b(bb), c(cc), d(dd), A(AA), B(BB), C(CC), D(DD)
 {
-    name = "Четырёхугольник";
-    sides_count = 4;
-    if (A + B + C + D != 180.0) {
-        throw FigureException("Сумма углов не равна 180");
+    if (A + B + C + D != 360.0) {
+        throw FigureException("РЎСѓРјРјР° СѓРіР»РѕРІ РЅРµ СЂР°РІРЅР° 360");
     }
+
+    name = "Р§РµС‚С‹СЂС‘С…СѓРіРѕР»СЊРЅРёРє";
+    sides_count = 4;
+
 }
 
 void Quadrangle::print_info()
 {
     Figure::print_info();
 
-    cout << "Стороны: a = " << a << ", b = " << b << ", c = " << c << ", d = " << d
-         << " Углы: A = " << A << ", B = " << B << ", C = " << C << ", D = " << D << ".\n" << endl;
+    cout << "РЎС‚РѕСЂРѕРЅС‹: a = " << a << ", b = " << b << ", c = " << c << ", d = " << d
+         << " РЈРіР»С‹: A = " << A << ", B = " << B << ", C = " << C << ", D = " << D << ".\n" << endl;
 }
