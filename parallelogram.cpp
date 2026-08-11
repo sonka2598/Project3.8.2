7760dcc8dd949470505ba8fed1f4eab82439e802
sonka2598 <69012990+sonka2598@users.noreply.github.com>
2026-08-11 23:05:47 +0600

    Update parallelogram.cpp

--- parallelogram.cpp
+++ parallelogram.cpp
@@ -3,5 +3,9 @@
 Parallelogram::Parallelogram(double a, double b, double A, double B)
     : Quadrangle(a, b, a, b, A, B, A, B)
 {
-    name = "��������������";
+    if (A + B != 180.0) {
+        throw FigureException("Это не параллелограмм.");
+    }
+
+    name = "Параллелограмм";
 }
